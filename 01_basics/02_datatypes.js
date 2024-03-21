ff8c3c448984a099fa341f449ed41eee3c0cac57
Alok Anand <101458638+Bhumialok@users.noreply.github.com>
2024-03-21 10:15:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+let name = "alok" //string
+let age = 20 //variable
+let isLoggedIn = false //boolean
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => it's a datatype as well as it has a standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "alok"); //string
+console.log(typeof null); //object
