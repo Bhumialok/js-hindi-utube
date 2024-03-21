9330e4fb5543d39691499fb5884362526530acf0
Alok Anand <101458638+Bhumialok@users.noreply.github.com>
2024-03-21 10:24:54 +0000

    rename

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "alokanand0909@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountGender;
+
+// accountId = 2 // not alllowed because of const datatypes
+
+accountEmail = "hc@gmail.com"
+accountPassword = "5645415"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountGender])
